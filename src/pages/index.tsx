f8749403d3f50f50f35b4d305865fba7a2b003a9
Kane Knight <kane.knight@hotmail.com>
2021-02-07 00:43:50 +0000

    uninstall stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,9 @@
 import React from "react";
-import { request } from "graphql-request";
-import { Navbar } from "../components/Navbar/Navbar"
-import { ArticleCard } from "../components/ArticleCard/ArticleCard";
-import { Grid, Segment, Header } from "semantic-ui-react";
 import { GetServerSideProps } from 'next'
-//import { Article } from "../types/types"
-import 'semantic-ui-css/semantic.min.css'
 import { withApollo } from '../withApollo';
 import { ssrArticles, PageArticlesComp } from '../generated/page';
-import { Article } from '../generated/graphql';
-
-/*const displayArticles = ( articles : Article[] ) => {
-    return articles.map(
-        (article) => {
-            return (
-                <Grid.Column key={article.id}>
-                    <ArticleCard article={article} />
-                </Grid.Column>
-            );
-        }
-    );
-};*/
 
 const Home : PageArticlesComp = ( { data , error } ) => {
-    {/*<div>
-        <Navbar page="home" />
-        <Segment attached="bottom" textAlign="center" color='blue'>
-            <Header as="h3"> Most Popular Articles </Header>
-        </Segment>
-        <Segment attached="bottom">
-            <Grid columns={4} stackable doubling={true} relaxed={true}>
-                { displayArticles( articles ) }
-            </Grid>
-        </Segment>
-    </div>*/}
     if ( error ) {
         return <p>Error times</p>
     }
